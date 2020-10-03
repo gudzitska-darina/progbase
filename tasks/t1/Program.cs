3a67608c015df39362c0b31825896b5ff95eda1d
gudzitska-darina <gudzitska.darina@gmail.com>
2020-10-03 13:55:12 +0300

    finish

---
+++ tasks/t1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace t1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("My name is Darina Hudzitska."); // да это я)
+            Console.WriteLine("My group is KP-02.");
+
+        }
+    }
+}
