5ea39f41a269b100da4eaf59a3fc7d592dc5b520
gudzitska-darina <gudzitska.darina@gmail.com>
2020-10-15 18:36:42 +0300

    lab1 finish

---
+++ labs/lab1/z1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace z1
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Write a ");
+            double a = double.Parse(Console.ReadLine());
+            Console.WriteLine("Write b ");
+            double b = double.Parse(Console.ReadLine());
+            Console.WriteLine("Write c ");
+            double c = double.Parse(Console.ReadLine());
+            if ((a - b) != 0 && a != 0 && Math.Sin(a) != 0)
+            {
+                double d0 = (Math.Pow((a + 3), (c + 1)) - 10) / (a - b);
+                double d1 = 5 * b + c / a;
+                double d2 = Math.Sqrt(Math.Abs(Math.Cos(b) / Math.Sin(a) + 5));
+
+                double d = d0 + d1 + d2;
+
+                Console.WriteLine("a = {0}", a);
+                Console.WriteLine("b = {0}", b);
+                Console.WriteLine("c = {0}", c);
+                Console.WriteLine("d0 = {0}", d0);
+                Console.WriteLine("d1 = {0}", d1);
+                Console.WriteLine("d2 = {0}", d2);
+                Console.WriteLine("d = {0}", d);
+            }
+            else
+            {
+                Console.WriteLine("Doesn`t correspond to ODZ!");
+            }
+        }
+    }
+}
---
+++ labs/lab1/z2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace z2
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Write x ");
+            double x = double.Parse(Console.ReadLine());
+            double y;
+
+            if(x>=0 && x<=8){
+            y= (Math.Pow(x,2)-5)/(x-1);
+            }
+            else if((x-1) == 0 ){
+            y= double.NaN;
+            }
+	        else{
+		    y= Math.Cos(Math.Pow(x,2)) / Math.Pow(Math.Sin(2*x), 2) + 1;
+            }	
+            
+
+            Console.WriteLine("y = {0}", y);
+        }
+    }
+}
