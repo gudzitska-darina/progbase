ed8d89dfb36cb3200c2fda2fadd615fe2020b8e9
gudzitska-darina <gudzitska.darina@gmail.com>
2020-10-28 21:51:11 +0200

    lab2 finish

---
+++ labs/lab2/z1/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using static System.Math;
+
+ class Program{
+
+    static double Fx(double x)
+    {
+        if(x>=-5 && x<=3)
+        {
+            return Gx(x);
+        }
+         else if( (x-1)==0)
+        {
+            return double.NaN;
+        }
+	    else
+        {
+	        return Hx(x);
+        }	
+    }
+    
+    static double Gx(double x)
+    {
+         return (Pow(x,2) - 5) / (x - 1);
+    }
+
+     static double Hx(double x)
+    {
+         return Cos(Pow(x,2)) / Pow(Sin(2*x), 2) + 1;
+    }
+
+     static void Main()
+     {
+        const double min = -10;
+        const double max = 10;
+        const double step = 0.5;
+        double x= min;
+        double y=0;
+        while(x<=max)
+        { 
+            y= Fx(x);
+    
+        Console.WriteLine("y[{0}] = {1}",x, y);
+        x=x+step;
+        }
+    }
+ }
---
+++ labs/lab2/z2/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using static System.Math;
+
+ class Program{
+    static void Main()
+    {
+        Console.WriteLine("Введите минимальное значение ");
+        double xMin = double.Parse(Console.ReadLine());
+        Console.WriteLine("Введите максимальное значение ");
+        double xMax = double.Parse(Console.ReadLine());
+        Console.WriteLine("Введите шаг ");
+        double nSteps = double.Parse(Console.ReadLine());
+            if(xMin>=xMax )
+            {
+                Console.WriteLine("Error (min|max)");
+            }
+            else if(nSteps<=0)
+            {
+                Console.WriteLine("Error (step)");
+            }
+            else
+            {
+                double integl = IntFx(xMin, xMax, nSteps);
+                Console.WriteLine("Интеграл: {0}", integl);
+            }
+
+    }   
+ 
+    static double Fx(double x)
+    {
+        if(x>=-5 && x<=3)
+        {
+            return Gx(x);
+        }
+         else if( (x-1)==0)
+        {
+            return double.NaN;
+        }
+	    else
+        {
+	        return Hx(x);
+        }	
+    }
+    
+    static double Gx(double x)
+    {
+         return (Pow(x,2) - 5) / (x - 1);
+    }
+
+    static double Hx(double x)
+    {
+         return Cos(Pow(x,2)) / Pow(Sin(2*x), 2) + 1;
+    }
+    
+    static double IntFx(double xMin, double xMax, double nSteps)
+    {
+        double step = (xMax-xMin)/nSteps;
+        double sum =0;
+        for(int i =0; i<=nSteps; i++ )
+        {
+            double x = xMin + i * step;
+            double y = Fx(x); 
+            sum =+ y;
+        }
+
+        double integl = step*sum;
+        return integl;
+
+    }
+}
