4b265c40e9013b86869c69c737791d80f8acb284
gudzitska-darina <gudzitska.darina@gmail.com>
2020-12-24 00:34:25 +0200

    finish lab3

---
+++ labs/lab3/z1/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace labaDarina
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] mountains = new int[int.Parse(Console.ReadLine())];
+            int[] mountains1 = new int[mountains.Length];
+
+            Random random = new Random();
+            for (int i = 0; i < mountains.Length; i++)
+            {
+                mountains[i] = random.Next(-10, 10);
+                Console.Write("{0,2} ", mountains[i]);
+            }
+            Console.WriteLine();
+            int min = Math.Abs(mountains.Min());
+            for (int i = 0; i < mountains.Length; i++)
+            {
+                mountains[i] += min;
+                Console.Write("{0,2} ", mountains[i]);
+            }
+            Console.WriteLine();
+            int waterLevel = int.Parse(Console.ReadLine());
+            
+            Print(mountains, mountains1, waterLevel);
+            
+            int vol=CountUnderwaterLandVolume(mountains1, waterLevel);
+            Console.WriteLine("Обьем самой большой горы: {0}",vol);
+        }
+
+        static int CountUnderwaterLandVolume(int[] heights, int waterLevel) 
+        { 
+            int vol=0;
+            int counter=0;
+            int[] changes = new int[heights.Length]; 
+            for (int i = 0; i < heights.Length; i++)
+            {
+                if (heights[i]!= 0) 
+                {
+                    counter += heights[i];
+                    changes[i]=counter;
+                }
+
+                else if (heights[i] == 0)
+                {
+                    counter=0;
+                } 
+            }            
+            vol = Math.Abs(changes.Max());
+            return vol;
+        } 
+
+        static void Print(int[] mountains, int[] mountains1, int waterLevel)
+        {
+            for (int i = 0; i < mountains.Length; i++)
+            {
+                if (mountains[i] - waterLevel < 0)
+                    mountains1[i] = 0;
+                else
+                    mountains1[i] = mountains[i] - waterLevel;
+                Console.Write("{0} ", mountains1[i]);
+            }
+            char[,] mount = new char[mountains.Max(),mountains.Length];
+            for (int i = 0; i < mount.GetLength(0); i++)
+            {
+                for (int j = 0; j < mount.GetLength(1); j++)
+                {
+                    if (mountains[j] >= i)
+                        mount[i, j] = 'N';
+                    else
+                        mount[i, j] = (i < waterLevel) ? '~' : ' ';
+
+                }
+            }
+            Console.WriteLine();
+            for (int i = mount.GetLength(0) - 1; i > 0; i--)
+            {
+                for (int j = 0; j < mount.GetLength(1); j++)
+                {
+                    Console.Write(mount[i, j]);
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ labs/lab3/z2/Program.cs
@@ -0,0 +1,201 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace z2
+{
+    class Program
+    {
+
+        static void Main()
+        {
+            int[,] mass = new int[6, 6];
+            Random rand = new Random();
+            for (int i = 0; i < mass.GetLength(0) - 1; i++)
+            {
+                for (int j = 0; j < mass.GetLength(1) - 1; j++)
+                {
+                    mass[i, j] = rand.Next(0, 2);
+                    Console.Write("{0} ", mass[i, j]);
+                }
+                Console.WriteLine();
+            }
+
+
+            int counter;
+            InvertMatrix(mass);
+            Console.WriteLine("".PadLeft(mass.GetLength(0), '='));
+            EnumerateOnes(mass, out counter);
+            Console.WriteLine("".PadLeft(mass.GetLength(0), '='));
+            int length = counter;
+            CreateCounters(length);
+            MassSwap(mass);
+            MergeOnes(mass, length);
+            Print(mass);            
+
+
+        }
+
+        static void InvertMatrix(int[,] matrix)
+        {
+            for (int i = 0; i < matrix.GetLength(0) - 1; i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1) - 1; j++)
+                {
+                    if (matrix[i, j] == 1)
+                    {
+
+                        matrix[i, j] = 0;
+                    }
+                    else if (matrix[i, j] == 0)
+                    {
+                        matrix[i, j] = 1;
+                    }
+                    //Console.Write("{0} ", matrix[i, j]);
+                }
+                //Console.WriteLine();
+            }
+        }
+
+        static int EnumerateOnes(int[,] matrix, out int counter)
+        {
+            counter = 1;
+            for (int i = 0; i < matrix.GetLength(0) - 1; i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1) - 1; j++)
+                {
+                    if (matrix[i, j] == 1)
+                    {
+                        matrix[i, j] = counter;
+                        counter++;
+                    }
+                        Console.Write("{0,2} ", matrix[i, j]);
+                }
+                Console.WriteLine();
+            }
+            return counter;
+
+        }
+
+        static int[] CreateCounters(int length)
+        {
+            int c = 0;
+            do
+            {
+                int[] countersL = new int[length];
+                for (int i = 0; length > i; i++)
+                {
+                    countersL[i] = i;
+                    Console.Write("{0} ", countersL[i]);
+                }
+                c++;
+                return countersL;
+            }
+            while (c < length - 1);
+        }
+        
+        static void MassSwap(int[,] Massive)
+        {
+            int changes = 0;
+            Console.WriteLine();
+            do
+            {
+                changes = 0;
+                for (int j = 0; j < Massive.GetLength(0) - 1; j++)
+                {
+                    for (int i = 0; i < Massive.GetLength(1) - 1; i++)
+                    {
+
+                        if (i - 1 >= 0)
+                        {
+                            if (Massive[i - 1, j] < Massive[i, j] && Massive[i - 1, j] != 0)
+                            {
+                                Massive[i, j] = Massive[i - 1, j];
+                                changes++;
+                            }
+                            
+                        }
+                        if (i <= Massive.GetLength(0))
+                        {
+                            if (Massive[i + 1, j] < Massive[i, j] && Massive[i + 1, j] != 0)
+                            {
+                                Massive[i, j] = Massive[i + 1, j];
+                                changes++;
+                            }
+                            
+                        }
+                        if (j - 1 >= 0)
+                        {
+                            if (Massive[i, j - 1] < Massive[i, j] && Massive[i, j - 1] != 0)
+                            {
+                                Massive[i, j] = Massive[i, j - 1];
+                                changes++;
+                            }
+                            
+                        }
+                        if (j <= Massive.GetLength(1))
+                        {
+                            if (Massive[i, j + 1] < Massive[i, j] && Massive[i, j + 1] != 0)
+                            {
+                                Massive[i, j] = Massive[i, j + 1];
+                                changes++;
+                            }
+                            
+                        }
+
+
+                    }
+                }
+            }
+            while (changes > 0);
+            Console.WriteLine();
+            for (int i = 0; i < Massive.GetLength(0); i++)
+            {
+                for (int j = 0; j < Massive.GetLength(1); j++) 
+                {
+                    if (i != Massive.GetLength(0) - 1 && j != Massive.GetLength(1) - 1)
+                        Console.Write("{0, 2} ", Massive[i, j]);
+                }
+                Console.WriteLine();
+            }
+                    
+        }
+
+        static void MergeOnes(int[,] a, int n)
+        {
+            int[] ac = new int[n];
+            for (int i = 0; i < a.GetLength(0) -1; i++)
+                for (int j = 0; j < a.GetLength(1) -1; j++)
+                {
+                    if (a[i, j] == 0)
+                    {
+                        continue;
+                    }
+                    else ac[a[i, j] - 1]++;
+                }
+            for (int i = 0; i < ac.Length; i++)
+            {
+                Console.Write("{0} ",ac[i]);
+            }
+             int max = Math.Abs(ac.Max());
+            
+            Console.WriteLine("\nОбьем самого большого резервуара воды: {0} ",max);
+        }
+
+        static void Print(int[,] a)
+        {
+            Console.WriteLine();
+            for (int i = 0; i < a.GetLength(0) -1; i++)
+            {
+                Console.Write("|");
+                for (int j = 0; j < a.GetLength(1) -1; j++)
+                    Console.Write((a[i, j] == 0) ? ' ' : 'N');
+            
+                Console.Write("|");
+                Console.WriteLine();
+            }
+        }
+    }
+}
